95e7b0880349f53a281dbdcf742c94bf82dcf31f
MURAVIAN\Anwar <office@muravian.com>
2021-10-17 02:23:11 +0300

    Updates

--- src/CiAuth.php
+++ src/CiAuth.php
@@ -257,8 +257,8 @@ class CiAuth
     {
         $key = getenv('JWT_SECRET');
         $iat = time();
-        $tte = (int)getenv('JWT_IAT');
-        $exp = $iat + ($tte * 3600);
+        $timeToExpire = getenv('JWT_EXP');
+        $exp = $iat + ((int) $timeToExpire * 3600);
 
         $payload = array(
             "iss"   => "Issuer of the " . getenv('JWT_ISS'),
@@ -284,13 +284,11 @@ class CiAuth
         }
     }
 
-    public function getProfile()
-    {
+    public function getProfile() {
 
     }
 
-    public function updateProfile()
-    {
+    public function updateProfile() {
 
     }
 
